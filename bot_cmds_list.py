7dd56e7315cd1fe51d54b0c52e0989d0982b8057
Egorprivet <124155851+Egorprivet@users.noreply.github.com>
2026-03-01 21:10:31 +0500

    Create bot_cmds_list.py

---
+++ bot_cmds_list.py
@@ -0,0 +1,8 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command='start', description='🚀 Главное меню'),
+    BotCommand(command='help', description='❓ Помощь'),
+]
+from aiogram.types import BotCommand
+
